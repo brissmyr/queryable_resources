09387c1cd51dd27cad72f4f09d56ef67e433a8bd
Johan Brissmyr <brissmyr@gmail.com>
2011-07-22 23:52:10 +0200

    Support for resources

--- lib/generators/rails/queryable_scaffold_controller_generator.rb
+++ lib/generators/rails/queryable_scaffold_controller_generator.rb
@@ -2,11 +2,10 @@ require 'rails/generators/rails/scaffold_controller/scaffold_controller_generato
 
 module Rails
   module Generators
-    class QueryableResourcesControllerGenerator < ScaffoldControllerGenerator
+    class QueryableScaffoldControllerGenerator < ScaffoldControllerGenerator
       def self.source_root
         @source_root ||= File.expand_path("templates", File.dirname(__FILE__))
       end
-      self.remove_hook_for :template_engine
     end
   end
 end
--- lib/queryable_resources.rb
+++ lib/queryable_resources.rb
@@ -1,4 +1,5 @@
 require 'rails/engine'
+#require 'rails/generators/rails/resource/resource_generator'
 
 module QueryableResources
   autoload :VERSION, 'queryable_resources/version'
@@ -6,9 +7,11 @@ module QueryableResources
   class Railtie < ::Rails::Engine
     config.queryable_resources = QueryableResources
     if config.respond_to?(:app_generators)
-      config.app_generators.scaffold_controller = :queryable_resources_controller
+      config.app_generators.scaffold_controller = :queryable_scaffold_controller
+      config.app_generators.resource_controller = :queryable_resources_controller
     else
-      config.generators.scaffold_controller = :queryable_resources_controller
+      config.generators.scaffold_controller = :queryable_scaffold_controller
+      config.generators.resource_controller = :queryable_resources_controller
     end
   end
 end
